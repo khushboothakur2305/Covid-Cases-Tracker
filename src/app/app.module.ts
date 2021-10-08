6fd78d9c5366fea6261b00d6d8f698d86180a5ed
khushboothakur2305 <khushboothakur2305@gmail.com>
2021-10-08 13:35:48 +0530

    feat(shared):integrate shared services and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
+import { SharedModule } from './shared/module/shared.module';
+import { LoaderComponent } from './shared/components/loader/loader.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
